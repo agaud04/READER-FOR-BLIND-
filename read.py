8ca082d60f1d15a2bedeb253f4f02257fcc0002c
skybrook <38181689+skybrook@users.noreply.github.com>
2018-10-05 15:05:20 +0530

    Add files via upload

---
+++ read.py
@@ -0,0 +1,12 @@
+from subprocess import call
+
+#cmd_img='fswebcam -r 720x1280 image2.png'#to capture image
+cmd_beg= ' tesseract abc.png xyz'#To convert image file to text file
+cmd_end= ' espeak -ven+f4 -f xyz.txt 2>/dev/null' # To play back from text file
+
+#call([cmd_img], shell=true)
+call([cmd_beg], shell=True)
+call([cmd_end], shell=True)
+
+
+
